5ece47987bba92368a15bc5b7f43d575f020c803
ArjunCodess <137415649+ArjunCodess@users.noreply.github.com>
2024-08-28 11:55:07 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ lib/utils.ts
@@ -0,0 +1,47 @@
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { EachRoute, ROUTES } from "./routes-config";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function helperSearch(
+  query: string,
+  node: EachRoute,
+  prefix: string,
+  currenLevel: number,
+  maxLevel?: number
+) {
+  const res: EachRoute[] = [];
+  let parentHas = false;
+
+  const nextLink = `${prefix}${node.href}`;
+  if (!node.noLink && node.title.toLowerCase().includes(query.toLowerCase())) {
+    res.push({ ...node, items: undefined, href: nextLink });
+    parentHas = true;
+  }
+  const goNext = maxLevel ? currenLevel < maxLevel : true;
+  if (goNext)
+    node.items?.forEach((item) => {
+      const innerRes = helperSearch(
+        query,
+        item,
+        nextLink,
+        currenLevel + 1,
+        maxLevel
+      );
+      if (!!innerRes.length && !parentHas && !node.noLink) {
+        res.push({ ...node, items: undefined, href: nextLink });
+        parentHas = true;
+      }
+      res.push(...innerRes);
+    });
+  return res;
+}
+
+export function advanceSearch(query: string) {
+  return ROUTES.map((node) =>
+    helperSearch(query, node, "", 1, query.length == 0 ? 2 : undefined)
+  ).flat();
+}
